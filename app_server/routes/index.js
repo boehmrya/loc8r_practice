2d343361c7cac43c25cf5b340d254f643c813924
Ryan Boehm <ryan@Ryans-Air.fios-router.home>
2019-09-22 22:23:54 -0400

    created callback function for home page route

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+const homepageController = (req, res) => {
   res.render('index', { title: 'Express' });
-});
+}
+router.get('/', homepageController);
 
 module.exports = router;
