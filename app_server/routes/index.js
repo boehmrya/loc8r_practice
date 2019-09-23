1d8f6557a3adb054929ea308ad5c3896fa2c65dd
Ryan <ryan.w.boehm@gmail.com>
2019-09-23 00:38:51 -0400

    included separate controller in route file for home page

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,10 +1,7 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const ctrlMain = require('../controllers/main');
 
 /* GET home page. */
-const homepageController = (req, res) => {
-  res.render('index', { title: 'Express' });
-}
-router.get('/', homepageController);
-
+router.get('/', ctrlMain.index);
 module.exports = router;
